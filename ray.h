076f065a15fab9db438318f25963dbd82c351630
Lakshya Vyas <181981038+vylakshya@users.noreply.github.com>
2025-08-06 16:50:33 +0530

    Update ray.h

--- ray.h
+++ ray.h
@@ -9,7 +9,7 @@ class Ray{
    
     public:
     
-    void GenRay(SDL_Renderer *ren,int x0, int y0, int x1, int y1, double r){
+    void GenRay(SDL_Renderer *ren,int x0, int y0, int x1, int y1, double r, int x_o, int y_o){
         std::vector<std::pair<float, float>> base_hyperbola;
         std::vector<std::pair<float,float>> obj;
     for (float t = 0.3; t < 50; t += 0.01)
@@ -20,6 +20,9 @@ for (float angle = 0; angle <= 2*PI; angle += 0.05) {
     for (auto [x, y] : base_hyperbola) {
         float x_rot = x * cos(angle) - y * sin(angle);
         float y_rot = x * sin(angle) + y * cos(angle);
+        
+        double dis = (x0 + x_rot - x_o)*(x0 + x_rot - x_o) + (y0 + y_rot - y_o)*(y0 + y_rot - y_o);
+        if(dis >= r*r) 
         SDL_RenderDrawPoint(ren, x0 + x_rot, y0 + y_rot);
     }
 }
